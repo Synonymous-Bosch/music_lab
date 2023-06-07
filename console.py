df31abaa724af54dccf412ad5f0500795e6b830d
Steven <stevenjohnmorgan@gmail.com>
2023-06-07 10:13:35 +0100

    adjusted order of console tests to provide clearer confirmation that methods were successful

--- console.py
+++ console.py
@@ -12,7 +12,7 @@ artist_repo.delete_all()
 artist1 = Artist("Godspeed You! Black Emperor")
 artist_repo.save(artist1)
 
-artist2 = Artist('Nervana')
+artist2 = Artist("Poo Man's Poison")
 artist_repo.save(artist2)
 
 
@@ -20,38 +20,40 @@ artist_repo.save(artist2)
 album1 = Album("G_d's Pee AT STATE'S END!", "Post-rock", artist1)
 album_repo.save(album1)
 
-album2 = Album("Nevermand", "Grunge Rock", artist2)
+album2 = Album("Providense", "Country", artist2)
 album_repo.save(album2)
 
 artists = artist_repo.select_all()
 albums = album_repo.select_all()
 
-artists[1].name = "Nirvana"
+album = album_repo.select(albums[1].id)
+
+artist = artist_repo.select(artists[1].id)
+
+artists[1].name = "Poor Man's Poison"
 artist_repo.update(artists[1])
 # pdb.set_trace()
 
-albums[1].title = "Nevermind"
+albums[1].title = "Providence"
 album_repo.update(albums[1])
 
 
-album = album_repo.select(albums[0].id)
 
-artist = artist_repo.select(artists[0].id)
 
 albums_by_artist = album_repo.find_albums_by_artist(artist2)
 
 album_repo.delete(albums[0].id)
 artist_repo.delete(artists[0].id)
 
-result = album_repo.select_all()
+album_result = album_repo.select_all()
 
-for item in result:
+for item in album_result:
     print(item.__dict__)
 
-result = artist_repo.select_all()
-for item in result:
+artist_result = artist_repo.select_all()
+for item in artist_result:
     print(item.__dict__)
 
-# pdb.set_trace()
+pdb.set_trace()
 
 
